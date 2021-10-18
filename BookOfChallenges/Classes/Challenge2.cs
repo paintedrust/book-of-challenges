2abacd172a8b43517e4af7a25d78f4a1273287da
johnny <JD01@live.co.uk>
2021-10-18 20:53:34 +0100

    test git on vs 2019

--- BookOfChallenges/Classes/Challenge2.cs
+++ BookOfChallenges/Classes/Challenge2.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 namespace BookOfChallenges.Classes
 {
-    public class Challenge2
+    public static class Challenge2
     {
-        public Challenge2()
+        public static void Run()
         {
+            float v = 9;
+            Console.WriteLine(v);
         }
     }
 }
